10761e78bacbac45131d04c38268b8fe0bb24d0f
ManhCuong304 <nmcuong3004@gmail.com~>
2025-06-08 11:22:04 +0700

    Giao Dien

---
+++ DoAnLapTrinhWeb/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using DoAnLapTrinhWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+
+namespace DoAnLapTrinhWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Profile()
+        {
+            // Dữ liệu mẫu, bạn có thể thay bằng dữ liệu lấy từ database
+            var model = new ProfileViewModel
+            {
+                FullName = "Nguyễn Văn A",
+                Nickname = "NVA",
+                PhoneNumber = "0123456789",
+                BirthDate = new DateTime(2000, 1, 1)
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Profile(ProfileViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Lưu dữ liệu vào database (nếu có)
+                ViewBag.Message = "Cập nhật thông tin thành công!";
+            }
+
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DoAnLapTrinhWeb/Models/ProfileViewModel.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DoAnLapTrinhWeb.Models
+{
+    public class ProfileViewModel
+    {
+        [Display(Name = "Họ và tên")]
+        [Required]
+        public string FullName { get; set; }
+
+        [Display(Name = "Biệt danh")]
+        public string Nickname { get; set; }
+
+        [Display(Name = "Số điện thoại")]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "Ngày sinh")]
+        [DataType(DataType.Date)]
+        public DateTime? BirthDate { get; set; }
+    }
+}
