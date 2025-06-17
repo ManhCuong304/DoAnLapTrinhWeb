2414cab0be583ff98172c5ec08f097710ebd8dda
ManhCuong304 <nmcuong3004@gmail.com~>
2025-06-17 15:20:07 +0700

    Lưu thay đổi tạm thời trước khi chuyển nhánh

--- DoAnLapTrinhWeb/obj/Debug/net8.0/DoAnLapTrinhWeb.AssemblyInfo.cs
+++ DoAnLapTrinhWeb/obj/Debug/net8.0/DoAnLapTrinhWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoAnLapTrinhWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10761e78bacbac45131d04c38268b8fe0bb24d0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoAnLapTrinhWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoAnLapTrinhWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
